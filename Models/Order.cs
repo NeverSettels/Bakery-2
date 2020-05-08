e13065e7352b0073e775305ffa8ba881ad1f54be
NeverSettels <settels.eric@gmail.com>
2020-05-08 15:17:25 -0700

    Claculate total price of pastry with discount and add totals

--- Models/Order.cs
+++ Models/Order.cs
@@ -27,14 +27,20 @@ namespace Bakery.Models
       int numBreads = BreadOrder.Count;
       int numPastries = PastryOrder.Count;
       int breadTotal = 0;
+      int pastryTotal = 0;
       int numFreeB = numBreads % 2;
+      int pastryDiscount = numPastries / 3;
 
       for (int i = 0; i < numBreads - numFreeB; i++)
       {
         breadTotal += BreadOrder[i].Price;
       }
-
-
+      foreach (Pastry pastry in PastryOrder)
+      {
+        pastryTotal += pastry.Price;
+      }
+      pastryTotal -= pastryDiscount;
+      return pastryTotal + breadTotal;
     }
   }
 
